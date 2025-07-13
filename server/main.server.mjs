a8408580f7a7324ac40d51766b610ad305d7e101
Fran Ibañez <ibanezvarelafran@gmail.com>
2025-07-13 12:41:23 +0200

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AAVE4RQF.mjs";import"./chunk-SMWIYOLC.mjs";import"./chunk-LMJQAGED.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-JQBW7HMJ.mjs";import"./chunk-SMWIYOLC.mjs";import"./chunk-LMJQAGED.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
