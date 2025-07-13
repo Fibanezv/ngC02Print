22a1d79259e0a96b03100e713b9f46f30aada0b2
Fran Ibañez <ibanezvarelafran@gmail.com>
2025-07-13 12:45:52 +0200

    Auto-generated commit

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-HAD6ZSVZ.mjs";import"./chunk-MCTNFIHB.mjs";import"./chunk-JW5MAGMA.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
--- server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{k as o,l as r,m as n}from"./chunk-MCTNFIHB.mjs";import{Wa as e}from"./chunk-JW5MAGMA.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
